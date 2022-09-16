17bd0b542be4a648c1483b9318c28b910c219e5f
Kamalani <kamalani@dev-island.org>
2022-09-15 15:44:06 -1000

    rating missing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const card1 = document.querySelector(".card1");
 const card2 = document.querySelector(".card2");
 const rating_btns = document.querySelectorAll(".rating_btns");
 const score = document.querySelector(".score");
+let rate = 0;
 
 btn_submit.addEventListener("click", onSubmit);
 rating_btns.forEach((rating_btn) => {
@@ -11,24 +12,21 @@ rating_btns.forEach((rating_btn) => {
 
 function onSubmit() {
   card1.classList.add("hide");
-  //   score.textContent = stars_score;
+  // score.textContent = stars_score;
   card2.classList.remove("hide");
   //   console.log("submit click");
 }
+
+// const evt = handleRatingBtnClick;
+
+// const displayRate = document.getElementsByClassName(".rating_btn");
 
 function handleRatingBtnClick(evt) {
   rating_btns.forEach((rating_btn) => {
     rating_btn.classList.remove("active");
   });
-
-  //   if (evt.target.classlist.contains("rating_btn")) {
-  //     evt.target.classList.add("active");
-  //   } else {
-  //     evt.target.parentElement.classList.add("active");
-  //   }
-  //   console.log("rating btn click");
 }
 
-// stars_score = evt.target.textContent;
-
-// console.log(rating_btns);
+function displayRate(evt) {
+  document.getElementByClass(".rating_btn").innerHTML = `${evt.rating_btn}`;
+}
